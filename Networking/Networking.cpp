fee41509d83dba549c95994a0b709e72496a811d
Jagger Nast <j.nast@hotmail.com>
2020-11-08 11:18:36 -0800

    Add project files.

---
+++ Networking/Networking.cpp
@@ -0,0 +1,12 @@
+﻿// Networking.cpp : Defines the entry point for the application.
+//
+
+#include "Networking.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
